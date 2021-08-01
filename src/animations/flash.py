79b05d42ee0767f1e735df975337ff49935d59a1
jkoppenhaver <34195522+jkoppenhaver@users.noreply.github.com>
2021-08-01 19:19:00 -0400

    Updated period argument to match seconds as units across all animations

--- src/animations/flash.py
+++ src/animations/flash.py
@@ -3,9 +3,9 @@ from animator_base import AnimatorBase
 class Flash(AnimatorBase):
     current_state = False
 
-    def __init__(self, np, color, period=1000):
+    def __init__(self, np, color, period=1):
         self.color = color
-        super().__init__(np, period=period)
+        super().__init__(np, period=period*1000)
 
     def update(self, timer):
         if(self.current_state):
