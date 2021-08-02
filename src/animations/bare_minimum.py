d46f21c8a533fdbfbdd9e6706edf5ed2f097d818
jkoppenhaver <34195522+jkoppenhaver@users.noreply.github.com>
2021-08-02 00:42:01 -0400

    Added bare minimum animation template

---
+++ src/animations/bare_minimum.py
@@ -0,0 +1,15 @@
+from animator_base import AnimatorBase
+
+# This class can be used as a starting point for new animations
+
+class BareMinimum(AnimatorBase):
+
+    def __init__(self, np):
+        # Add any initialization and variables you need here
+        # You can also pass a different period to the Animator Base if you need to
+        super().__init__(np)
+
+    def update(self, timer):
+        # This update function is called at regular intervals by the Animator Base
+        # Replace 'pass' with your pattern updates and np.write()
+        pass
